7c9b41c8d3f72a31ea6af58ab57367e59a39b46e
Omar Mekky <omar.mekky@mashsolvents.com>
2014-12-20 18:03:05 +0200

    Version 0.1

---
+++ README.markdown
@@ -0,0 +1,53 @@
+Powerline Prayer Times
+======================
+
+**Author:** [Omar Mekky](http://cousine.me) - [iam@cousine.me](mailto: iam@cousine.me)
+
+**Powerline Prayer Times is a [Powerline](https://github.com/powerline/powerline)
+3rd party segment to show a countdown timer for the next due islamic prayer using
+[Al Adhan's](http://aladhan.com) REST API**
+
+Installation
+------------
+
+There isn't any automated method to install 3rd party segments for Powerline, however
+by following the steps below you can manually install Prayer Times:
+
+# Follow Powerline's [Quick setup guide](https://powerline.readthedocs.org/en/master/configuration.html#quick-setup-guide) to create the default configrations and folders
+# Under your `~/.config/powerline` directory, create a `segments` directory to hold 3rd party segments
+# Copy `pryr.py` to the newly created `segments` directory
+# Using your favorite editor open up `~/.config/powerline/config.json` and add the lines below to the `common` section:
+  `
+  "paths": [
+    "~/.config/powerline/segments"
+  ]
+  `
+# Now open `~/.config/powerline/themes/powerline.json` and add the following lines right after the last section:
+  `
+  ,"pryr.prayer_time": {
+    "args": {
+      "icons": {
+        "light":       "☼",
+        "dark":        "☾"
+      }
+    }
+  }
+  `
+# Next open up `~/.config/powerline/colorschemes` and add the following lines to the end of the `groups` section:
+  `
+  ,"prayer_times":              { "fg": "green", "bg": "gray2", "attr": [] },
+  "prayer_times_critical":     { "fg": "red", "bg": "gray2", "attr": [] },
+  "prayer_times_warning":    { "fg": "brightyellow", "bg": "gray2", "attr": [] }
+  `
+# Finally add `Prayer times` to your `Powerline` theme:
+`
+{
+  "function": "pryr.prayer_time",
+  "priority": 50,
+  "args": {
+    "location_query": "cairo, eg",
+    "timezone": 2,
+    "method": 5
+  }
+}
+`
---
+++ pryr.py
@@ -0,0 +1,158 @@
+# vim:filenecoding=utf-8:noet
+
+# A prayer times segment for Powerline (https://github.com/powerline/powerline/)
+# 
+# This segment is based on the official weather segment for Powerline
+# 
+# Author: Omar Mekky (http://cousine.me)
+# Date: 20-12-2014
+from __future__ import (unicode_literals, division, absolute_import, print_function)
+
+import json
+import time
+import math
+
+from datetime import datetime, timedelta
+
+from powerline.lib.url import urllib_read, urllib_urlencode
+from powerline.lib.threaded import KwThreadedSegment
+from powerline.segments import with_docstring
+
+class PrayerTimeSegment(KwThreadedSegment):
+    interval = 600
+    current_date_time = datetime.today()
+    default_location = None
+    location_geometries = {}
+    prayer_times = {}
+
+    @staticmethod
+    def key(location_query=None, timezone=2, method=5, **kwargs):
+        return (location_query, timezone, method)
+
+    def get_geometry(self, location_query):
+        try:
+            return self.location_geometries[location_query]
+        except KeyError:
+            if location_query is None:
+                self.error('No location specified')
+            else:
+                location = location_query.split(',')
+                query_data = {
+                        'address':  location[0],
+                        'region':   location[1],
+                        'sensor':   'false'
+                }
+
+                location_data = json.loads(urllib_read('http://maps.google.com/maps/api/geocode/json?' + urllib_urlencode(query_data)))
+                location = ','.join((
+                    str(location_data['results'][0]['geometry']['location']['lat']), 
+                    str(location_data['results'][0]['geometry']['location']['lng'])
+                ))
+
+                self.location_geometries[location_query] = geometry = location
+                return geometry
+
+    def get_prayer_times(self, prayer_tuple):
+        timestamp = long(math.ceil(time.time()))
+
+        if (datetime.fromtimestamp(timestamp) - self.current_date_time).days == 0 and self.prayer_times:
+            return self.prayer_times
+
+        geometry = self.get_geometry(prayer_tuple[0]).split(',')
+        query_data = {
+            'latitude': geometry[0],
+            'longitude': geometry[1],
+            'timezone': prayer_tuple[1],
+            'method': prayer_tuple[2]
+        }
+
+        raw_response = urllib_read('http://api.aladhan.com/timings/{0}?'.format(timestamp) + urllib_urlencode(query_data))
+
+        if not raw_response:
+            self.error('Failed to get response')
+            return None
+
+        response = json.loads(raw_response)
+        try:
+            for prayer, rtime in response['data']['timings'].iteritems():
+                ptime = datetime.strptime(rtime, '%H:%M')
+                self.prayer_times[prayer] = ptime.replace(self.current_date_time.year, self.current_date_time.month, self.current_date_time.day)
+        except (KeyError, ValueError):
+            self.exception('Al Adhan returned a malformed or unrexpected response: {0}', repr(raw_response))
+            return None
+
+        return self.prayer_times
+
+    def compute_state(self, prayer_tuple):
+        time_now = datetime.now()
+        present_prayer = None
+
+        if self.get_prayer_times(prayer_tuple):
+            for prayer, ptime in self.prayer_times.iteritems():
+                if time_now >= ptime:
+                    present_prayer = prayer
+                elif time_now < ptime and present_prayer:
+                    delta_time = ptime - time_now
+                    hours, remainder = divmod(delta_time.seconds, 3600)
+                    minutes, seconds = divmod(remainder, 60)
+
+                    if prayer == "Maghrib" or prayer == "Isha":
+                        icon_name = "dark"
+                    else:
+                        icon_name = "light"
+
+                    return (prayer, hours, minutes, icon_name)
+        else:
+            return None
+
+    def render_one(self, prayer_time, icons=None, **kwargs):
+        if not prayer_time:
+            return None
+        
+        prayer, hours, minutes, icon_name = prayer_time
+
+        if icons:
+            if icon_name in icons:
+                icon = icons[icon_name]
+            else:
+                icon = ''
+        else:
+            icon = ''
+
+        if hours == 0 and minutes < 30:
+            highlight_group = ['prayer_times_critical', 'prayer_times']
+        elif hours == 0 and minutes >= 30:
+            highlight_group = ['prayer_times_warning', 'prayer_times']
+        else:
+            highlight_group = ['prayer_times']
+
+        prayer_time_countdown = "{0} -{1:02}:{2:02}".format(prayer, hours, minutes)
+
+        return [
+            {
+                'contents': icon + ' ',
+                'highlight_group': highlight_group,
+                'divider_highlight_group': 'background:divider'
+            },
+            {
+                'contents': prayer_time_countdown,
+                'highlight_group': highlight_group,
+                'divider_highlight_group': 'background:divider'
+            }
+        ]
+
+prayer_time = with_docstring(PrayerTimeSegment(), 
+''' Return a countdown to the next prayer from Al Adhan (http://aladhan.com)
+
+:param str location_query:
+    location query for your current location, e.g. ``cairo, eg``
+:param int timezone:
+    your timezone in int, valid values are from -12 to 12
+:param int method:
+    prayer time calculation method, valid values are from 0 to 7 (for more information check http://aladhan.com/rest-api)
+
+Divider highlight group used: ``background:divider``
+
+Highlight groups used: ``prayer_times``, ``prayer_times_critical``, ``prayer_times_warning``
+'''
+)
